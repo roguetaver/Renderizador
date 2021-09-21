de1cfe0fa7b7e58ac1768d9769e1696ac1867787
decorocco <andrebr@al.insper.edu.br>
2021-09-21 06:09:10 -0300

    entrega 2

--- renderizador/gl.py
+++ renderizador/gl.py
@@ -4,12 +4,15 @@
 """
 Biblioteca Gráfica / Graphics Library.
 
-Desenvolvido por: <SEU NOME AQUI>
+Desenvolvido por: André Tavernaro e André Rocco
 Disciplina: Computação Gráfica
-Data:
+Data: 21/09/2021
 """
 
 import gpu          # Simula os recursos de uma GPU
+import numpy as np
+import math
+
 
 class GL:
     """Classe que representa a biblioteca gráfica (Graphics Library)."""
@@ -28,6 +31,32 @@ class GL:
         GL.far = far
 
     @staticmethod
+    def triangleSet2D(vertices, ponto, colors):
+        x, y = ponto[0], ponto[1]
+
+        x1, x2, x3 = vertices[0][0][0], vertices[1][0][0], vertices[2][0][0]
+        y1, y2, y3 = vertices[0][1][0], vertices[1][1][0], vertices[2][1][0]
+
+        L0 = False
+        L1 = False
+        L2 = False
+        x = x + 0.5
+        y = y + 0.5
+
+        if (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1) >= 0:
+            L0 = True
+
+        if (x - x2) * (y3 - y2) - (y - y2) * (x3 - x2) >= 0:
+            L1 = True
+
+        if (x - x3) * (y1 - y3) - (y - y3) * (x1 - x3) >= 0:
+            L2 = True
+
+        if(L1 and L2 and L0):
+            gpu.GPU.set_pixel(int(x), int(y), int(colors["diffuseColor"][0]*255), int(
+                colors["diffuseColor"][1]*255), int(colors["diffuseColor"][2]*255))
+
+    @staticmethod
     def triangleSet(point, colors):
         """Função usada para renderizar TriangleSet."""
         # Nessa função você receberá pontos no parâmetro point, esses pontos são uma lista
@@ -41,12 +70,36 @@ class GL:
         # O parâmetro colors é um dicionário com os tipos cores possíveis, para o TriangleSet
         # você pode assumir o desenho das linhas com a cor emissiva (emissiveColor).
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("TriangleSet : pontos = {0}".format(point)) # imprime no terminal pontos
-        print("TriangleSet : colors = {0}".format(colors)) # imprime no terminal as cores
+        GL.matriz_M = np.matmul(
+            np.matmul(GL.matriz_trans_in, GL.matriz_rotacao), GL.matriz_escala_in)
 
-        # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
+        GL.matriz_tela = np.array([[GL.width/2, 0, 0, GL.width/2],
+                                   [0, -GL.height/2, 0, GL.height/2],
+                                   [0, 0, 1, 0],
+                                   [0, 0, 0, 1]])
+        coords = []
+
+        GL.matriz_look_at = np.matmul(np.linalg.inv(
+            GL.matriz_orien_cam), np.linalg.inv(GL.matriz_trans_cam))
+
+        for i in range(0, len(point), 3):
+            ponto = np.array([[point[i]],
+                              [point[i+1]],
+                              [point[i+2]],
+                              [1]])
+
+            ponto = np.matmul(GL.matriz_M, ponto)
+            ponto = np.matmul(GL.matriz_look_at, ponto)
+            ponto = np.matmul(GL.matriz_perspectiva, ponto)
+            ponto /= ponto[3][0]
+            ponto = np.matmul(GL.matriz_tela, ponto)
+            coords.append(ponto)
+
+        for triangulo in range(0, len(coords), 3):
+            for i in range(GL.width):
+                for j in range(GL.height):
+                    GL.triangleSet2D(
+                        coords[triangulo:triangulo+3], [i, j], colors)
 
     @staticmethod
     def viewpoint(position, orientation, fieldOfView):
@@ -55,11 +108,42 @@ class GL:
         # câmera virtual. Use esses dados para poder calcular e criar a matriz de projeção
         # perspectiva para poder aplicar nos pontos dos objetos geométricos.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("Viewpoint : ", end='')
-        print("position = {0} ".format(position), end='')
-        print("orientation = {0} ".format(orientation), end='')
-        print("fieldOfView = {0} ".format(fieldOfView))
+        fovy = 2*math.atan(math.tan(fieldOfView/2)*GL.height /
+                           math.sqrt(GL.height**2 + GL.width**2))
+        cima = GL.near*math.tan(fovy)
+        direita = cima*(GL.width/GL.height)
+
+        if orientation:
+            if (orientation[0] > 0):
+                GL.matriz_orien_cam = np.array([[1, 0, 0, 0],
+                                                [0, math.cos(
+                                                    orientation[3]), -math.sin(orientation[3]), 0],
+                                                [0, math.sin(orientation[3]), math.cos(
+                                                    orientation[3]), 0],
+                                                [0, 0, 0, 1]])
+            elif (orientation[1] > 0):
+                GL.matriz_orien_cam = np.array([[math.cos(orientation[3]), 0, math.sin(orientation[3]), 0],
+                                                [0, 1, 0, 0],
+                                                [-math.sin(orientation[3]), 0,
+                                                 math.cos(orientation[3]), 0],
+                                                [0, 0, 0, 1]])
+            else:
+                GL.matriz_orien_cam = np.array([[math.cos(orientation[3]), -math.sin(orientation[3]), 0, 0],
+                                                [math.sin(orientation[3]), math.cos(
+                                                    orientation[3]), 0, 0],
+                                                [0, 0, 1, 0],
+                                                [0, 0, 0, 1]])
+
+        GL.matriz_perspectiva = np.array([[GL.near/direita, 0, 0, 0],
+                                          [0, GL.near/cima, 0, 0],
+                                          [0, 0, -((GL.far+GL.near)/(GL.far-GL.near)), -
+                                           ((2*GL.far*GL.near)/(GL.far-GL.near))],
+                                          [0, 0, -1, 0]])
+
+        GL.matriz_trans_cam = np.array([[1, 0, 0, position[0]],
+                                        [0, 1, 0, position[1]],
+                                        [0, 0, 1, position[2]],
+                                        [0, 0, 0, 1]])
 
     @staticmethod
     def transform_in(translation, scale, rotation):
@@ -72,15 +156,38 @@ class GL:
         # Quando se entrar em um nó transform se deverá salvar a matriz de transformação dos
         # modelos do mundo em alguma estrutura de pilha.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("Transform : ", end='')
-        if translation:
-            print("translation = {0} ".format(translation), end='') # imprime no terminal
-        if scale:
-            print("scale = {0} ".format(scale), end='') # imprime no terminal
         if rotation:
-            print("rotation = {0} ".format(rotation), end='') # imprime no terminal
-        print("")
+            if (rotation[0] > 0):
+                GL.matriz_rotacao = np.array([[1, 0, 0, 0],
+                                              [0, math.cos(
+                                                  rotation[3]), -math.sin(rotation[3]), 0],
+                                              [0, math.sin(rotation[3]), math.cos(
+                                                  rotation[3]), 0],
+                                              [0, 0, 0, 1]])
+            elif (rotation[1] > 0):
+                GL.matriz_rotacao = np.array([[math.cos(rotation[3]), 0, math.sin(rotation[3]), 0],
+                                              [0, 1, 0, 0],
+                                              [-math.sin(rotation[3]), 0,
+                                               math.cos(rotation[3]), 0],
+                                              [0, 0, 0, 1]])
+            else:
+                GL.matriz_rotacao = np.array([[math.cos(rotation[3]), -math.sin(rotation[3]), 0, 0],
+                                              [math.sin(rotation[3]), math.cos(
+                                                  rotation[3]), 0, 0],
+                                              [0, 0, 1, 0],
+                                              [0, 0, 0, 1]])
+
+        if scale:
+            GL.matriz_escala_in = np.array([[scale[0], 0, 0, 0],
+                                            [0, scale[1], 0, 0],
+                                            [0, 0, scale[2], 0],
+                                            [0, 0, 0, 1]])
+
+        if translation:
+            GL.matriz_trans_in = np.array([[1, 0, 0, translation[0]],
+                                           [0, 1, 0, translation[1]],
+                                           [0, 0, 1, translation[2]],
+                                           [0, 0, 0, 1]])
 
     @staticmethod
     def transform_out():
@@ -107,15 +214,30 @@ class GL:
         # depois 2, 3 e 4, e assim por diante. Cuidado com a orientação dos vértices, ou seja,
         # todos no sentido horário ou todos no sentido anti-horário, conforme especificado.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("TriangleStripSet : pontos = {0} ".format(point), end='')
-        for i, strip in enumerate(stripCount):
-            print("strip[{0}] = {1} ".format(i, strip), end='')
-        print("")
-        print("TriangleStripSet : colors = {0}".format(colors)) # imprime no terminal as cores
+        coords = []
+        GL.matriz_M = np.matmul(
+            np.matmul(GL.matriz_trans_in, GL.matriz_rotacao), GL.matriz_escala_in)
+        GL.matriz_look_at = np.matmul(np.linalg.inv(
+            GL.matriz_orien_cam), np.linalg.inv(GL.matriz_trans_cam))
 
-        # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
+        for i in range(0, len(point), 3):
+            ponto = np.array([[point[i]],
+                              [point[i+1]],
+                              [point[i+2]],
+                              [1]])
+
+            ponto = np.matmul(GL.matriz_M, ponto)
+            ponto = np.matmul(GL.matriz_look_at, ponto)
+            ponto = np.matmul(GL.matriz_perspectiva, ponto)
+            ponto /= ponto[3][0]
+            ponto = np.matmul(GL.matriz_tela, ponto)
+            coords.append(ponto)
+
+        for s in range(stripCount[0]):
+            for x in range(GL.width):
+                for y in range(GL.height):
+                    if (s+3 < len(coords)):
+                        GL.triangleSet2D(coords[s:s+3], [x, y], colors)
 
     @staticmethod
     def indexedTriangleStripSet(point, index, colors):
@@ -132,12 +254,38 @@ class GL:
         # depois 2, 3 e 4, e assim por diante. Cuidado com a orientação dos vértices, ou seja,
         # todos no sentido horário ou todos no sentido anti-horário, conforme especificado.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("IndexedTriangleStripSet : pontos = {0}, index = {1}".format(point, index))
-        print("IndexedTriangleStripSet : colors = {0}".format(colors)) # imprime as cores
+        GL.matriz_M = np.matmul(
+            np.matmul(GL.matriz_trans_in, GL.matriz_rotacao), GL.matriz_escala_in)
 
-        # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
+        GL.matriz_tela = np.array([[GL.width/2, 0, 0, GL.width/2],
+                                   [0, -GL.height/2, 0, GL.height/2],
+                                   [0, 0, 1, 0],
+                                   [0, 0, 0, 1]])
+
+        coords = []
+
+        GL.matriz_look_at = np.matmul(np.linalg.inv(
+            GL.matriz_orien_cam), np.linalg.inv(GL.matriz_trans_cam))
+
+        for i in range(0, len(point), 3):
+            ponto = np.array([[point[i]],
+                              [point[i+1]],
+                              [point[i+2]],
+                              [1]])
+
+            ponto = np.matmul(GL.matriz_M, ponto)
+            ponto = np.matmul(GL.matriz_look_at, ponto)
+            ponto = np.matmul(GL.matriz_perspectiva, ponto)
+            ponto /= ponto[3][0]
+            ponto = np.matmul(GL.matriz_tela, ponto)
+            coords.append(ponto)
+
+        for ind in index:
+            if (index[ind+2] == -1):
+                break
+            for x in range(GL.width):
+                for y in range(GL.height):
+                    GL.triangleSet2D(coords[ind:ind+3], [x, y], colors)
 
     @staticmethod
     def box(size, colors):
@@ -145,16 +293,30 @@ class GL:
         # A função box é usada para desenhar paralelepípedos na cena. O Box é centrada no
         # (0, 0, 0) no sistema de coordenadas local e alinhado com os eixos de coordenadas
         # locais. O argumento size especifica as extensões da caixa ao longo dos eixos X, Y
-        # e Z, respectivamente, e cada valor do tamanho deve ser maior que zero. Para desenha
+        # e Z, respectivamente, e cada valor do tamanho deve ser maior que zero. Para desenhar
         # essa caixa você vai provavelmente querer tesselar ela em triângulos, para isso
         # encontre os vértices e defina os triângulos.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("Box : size = {0}".format(size)) # imprime no terminal pontos
-        print("Box : colors = {0}".format(colors)) # imprime no terminal as cores
+        x, y, z = size[0], size[1], size[2]
 
-        # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
+        ponto1 = (-x, -y, z)
+        ponto2 = (x, -y, z)
+        ponto3 = (x, y, z)
+        ponto4 = (-x, y, z)
+        ponto5 = (-x, y, -z)
+        ponto6 = (x, y, -z)
+        ponto7 = (-x, -y, -z)
+        ponto8 = (x, -y, -z)
+
+        pontos = [
+            ponto1, ponto2, ponto3, ponto3, ponto4, ponto1, ponto7, ponto1, ponto4,
+            ponto4, ponto5, ponto7, ponto8, ponto7, ponto5, ponto5, ponto6, ponto8,
+            ponto8, ponto6, ponto2, ponto2, ponto6, ponto3, ponto6, ponto5, ponto4,
+            ponto4, ponto3, ponto6, ponto8, ponto7, ponto1, ponto1, ponto2, ponto8
+        ]
+
+        pontos = list(sum(pontos, ()))
+        GL.triangleSet(pontos, colors)
 
     @staticmethod
     def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex,
@@ -182,25 +344,19 @@ class GL:
         # Os prints abaixo são só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
         print("IndexedFaceSet : ")
         if coord:
-            print("\tpontos(x, y, z) = {0}, coordIndex = {1}".format(coord, coordIndex))
-        print("colorPerVertex = {0}".format(colorPerVertex))
-        if colorPerVertex and color and colorIndex:
-            print("\tcores(r, g, b) = {0}, colorIndex = {1}".format(color, colorIndex))
-        if texCoord and texCoordIndex:
-            print("\tpontos(u, v) = {0}, texCoordIndex = {1}".format(texCoord, texCoordIndex))
+            print("\tpontos(x, y, z) = {0}, coordIndex = {1}".format(
+                coord, coordIndex))
+        if colorPerVertex:
+            print("\tcores(r, g, b) = {0}, colorIndex = {1}".format(
+                color, colorIndex))
+        if texCoord:
+            print("\tpontos(u, v) = {0}, texCoordIndex = {1}".format(
+                texCoord, texCoordIndex))
         if current_texture:
             image = gpu.GPU.load_texture(current_texture[0])
             print("\t Matriz com image = {0}".format(image))
-            print("\t Dimensões da image = {0}".format(image.shape))
-        print("IndexedFaceSet : colors = {0}".format(colors))  # imprime no terminal as cores
+        print("IndexedFaceSet : colors = {0}".format(
+            colors))  # imprime no terminal as cores
 
         # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
-
-
-    # Para o futuro (Não para versão atual do projeto.)
-    def vertex_shader(self, shader):
-        """Para no futuro implementar um vertex shader."""
-
-    def fragment_shader(self, shader):
-        """Para no futuro implementar um fragment shader."""
+        # gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
