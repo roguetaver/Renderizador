39b06ec6db9e8d063627ec6ac472b07f3330446b
roguetaver <andretavernaro@gmail.com>
2021-10-14 00:25:35 -0300

    entrega 3

--- renderizador/gl.py
+++ renderizador/gl.py
@@ -6,7 +6,7 @@ Biblioteca Gráfica / Graphics Library.
 
 Desenvolvido por: André Tavernaro e André Rocco
 Disciplina: Computação Gráfica
-Data: 21/09/2021
+Data: 14/10/2021
 """
 
 import gpu          # Simula os recursos de uma GPU
@@ -29,6 +29,7 @@ class GL:
         GL.height = height
         GL.near = near
         GL.far = far
+        GL.stack = []
 
     @staticmethod
     def triangleSet2D(vertices, ponto, colors):
@@ -197,8 +198,8 @@ class GL:
         # deverá recuperar a matriz de transformação dos modelos do mundo da estrutura de
         # pilha implementada.
 
-        # O print abaixo é só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("Saindo de Transform")
+        if (len(GL.stack) > 0):
+            GL.stack.pop()
 
     @staticmethod
     def triangleStripSet(point, stripCount, colors):
@@ -319,6 +320,58 @@ class GL:
         GL.triangleSet(pontos, colors)
 
     @staticmethod
+    def pintaDentro(vertices, ponto, colors, color, colorPerVertex, texture=False, texVertex=None, img=None):
+
+        x, y = ponto[0], ponto[1]
+
+        x1, x2, x3 = vertices[0][0][0], vertices[1][0][0], vertices[2][0][0]
+        y1, y2, y3 = vertices[0][1][0], vertices[1][1][0], vertices[2][1][0]
+
+        L0 = False
+        L1 = False
+        L2 = False
+        x = x + 0.5
+        y = y + 0.5
+
+        if (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1) >= 0:
+            L0 = True
+
+        if (x - x2) * (y3 - y2) - (y - y2) * (x3 - x2) >= 0:
+            L1 = True
+
+        if (x - x3) * (y1 - y3) - (y - y3) * (x1 - x3) >= 0:
+            L2 = True
+
+        r = int(colors["diffuseColor"][0]*255)
+        g = int(colors["diffuseColor"][1]*255)
+        b = int(colors["diffuseColor"][2]*255)
+
+        if(L1 and L2 and L0):
+            alpha = (-(x - x2)*(y3 - y2) + (y - y2)*(x3 - x2)) / \
+                (-(x1 - x2)*(y3 - y2) + (y1 - y2)*(x3 - x2))
+            betha = (-(x - x3)*(y1 - y3) + (y - y3)*(x1 - x3)) / \
+                (-(x2 - x3)*(y1 - y3) + (y2 - y3)*(x1 - x3))
+            gamma = 1 - (alpha + betha)
+
+            if colorPerVertex and color:
+                r = (color[0][0]*alpha + color[1][0]*betha + color[2][0]*gamma)
+                g = (color[0][1]*alpha + color[1][1]*betha + color[2][1]*gamma)
+                b = (color[0][2]*alpha + color[1][2]*betha + color[2][2]*gamma)
+                r = r * 255
+                g = g * 255
+                b = b * 255
+
+            if texture:
+                tex_x = (texVertex[0][0]*alpha + texVertex[1][0]
+                         * betha + texVertex[2][0]*gamma)*img.shape[0]
+                tex_y = (texVertex[0][1]*alpha + texVertex[1][1]
+                         * betha + texVertex[2][1]*gamma)*img.shape[1]
+
+                r, g, b, f = img[int(-tex_y)][int(tex_x)]
+
+            gpu.GPU.draw_pixels([int(x), int(y)], gpu.GPU.RGB8, [r, g, b])
+
+    @staticmethod
     def indexedFaceSet(coord, coordIndex, colorPerVertex, color, colorIndex,
                        texCoord, texCoordIndex, colors, current_texture):
         """Função usada para renderizar IndexedFaceSet."""
@@ -339,24 +392,84 @@ class GL:
         # baricêntrico de que cor deverá ter aquela posição. Da mesma forma se pode definir uma
         # textura para o poligono, para isso, use as coordenadas de textura e depois aplique a
         # cor da textura conforme a posição do mapeamento. Dentro da classe GPU já está
-        # implementadado um método para a leitura de imagens.
+        # implementadado um método para a leitura de imagens
 
-        # Os prints abaixo são só para vocês verificarem o funcionamento, DEVE SER REMOVIDO.
-        print("IndexedFaceSet : ")
-        if coord:
-            print("\tpontos(x, y, z) = {0}, coordIndex = {1}".format(
-                coord, coordIndex))
-        if colorPerVertex:
-            print("\tcores(r, g, b) = {0}, colorIndex = {1}".format(
-                color, colorIndex))
-        if texCoord:
-            print("\tpontos(u, v) = {0}, texCoordIndex = {1}".format(
-                texCoord, texCoordIndex))
+        tri = []
+        colorAbs = []
+        rgb = []
+        text = []
+
+        GL.matriz_M = np.matmul(
+            np.matmul(GL.matriz_trans_in, GL.matriz_rotacao), GL.matriz_escala_in)
+
+        if (len(GL.stack) > 1):
+            GL.matriz_M = np.matmul(GL.stack[-1], GL.matriz_M)
+
         if current_texture:
             image = gpu.GPU.load_texture(current_texture[0])
-            print("\t Matriz com image = {0}".format(image))
-        print("IndexedFaceSet : colors = {0}".format(
-            colors))  # imprime no terminal as cores
 
-        # Exemplo de desenho de um pixel branco na coordenada 10, 10
-        # gpu.GPU.draw_pixels([10, 10], gpu.GPU.RGB8, [255, 255, 255])  # altera pixel
+        GL.stack.append(GL.matriz_M)
+
+        GL.matriz_tela = np.array(
+            [[GL.width/2, 0, 0, GL.width/2], [0, -GL.height/2, 0, GL.height/2], [0, 0, 1, 0], [0, 0, 0, 1]])
+        vertex = []
+
+        GL.matriz_look_at = np.matmul(np.linalg.inv(
+            GL.matriz_orien_cam), np.linalg.inv(GL.matriz_trans_cam))
+
+        for i in range(0, len(coord), 3):
+            ponto = np.array([[coord[i]], [coord[i+1]], [coord[i+2]], [1]])
+
+            ponto = np.matmul(GL.matriz_M, ponto)
+            ponto = np.matmul(GL.matriz_look_at, ponto)
+            ponto = np.matmul(GL.matriz_perspectiva, ponto)
+            ponto /= ponto[3][0]
+            ponto = np.matmul(GL.matriz_tela, ponto)
+            vertex.append(ponto)
+
+        if coord and coordIndex:
+            for v in coordIndex:
+                if v < 0:
+                    for x in range(GL.width):
+                        for y in range(GL.height):
+                            GL.pintaDentro(
+                                tri, [x, y], colors, colorAbs, colorPerVertex)
+
+                    colorAbs = []
+                    tri = []
+                    continue
+                tri.append(vertex[v])
+
+        if colorPerVertex and colorIndex:
+
+            for c in range(0, len(color), 3):
+                rgb.append([color[c], color[c+1], color[c+2]])
+
+            for v, c in zip(coordIndex, colorIndex):
+                if v < 0 or c < 0:
+                    for x in range(GL.width):
+                        for y in range(GL.height):
+                            GL.pintaDentro(
+                                tri, [x, y], colors, colorAbs, colorPerVertex)
+
+                    colorAbs = []
+                    tri = []
+                    continue
+                colorAbs.append(rgb[c])
+                tri.append(vertex[v])
+
+        if (texCoord and texCoordIndex):
+            textura = []
+            for p in range(0, len(texCoord), 2):
+                textura.append([texCoord[p], texCoord[p+1]])
+            text = []
+            for v, t in zip(coordIndex, texCoordIndex):
+                if v < 0:
+                    for x in range(GL.width):
+                        for y in range(GL.height):
+                            GL.pintaDentro(tri, [
+                                           x, y], colors, colorAbs, colorPerVertex, texture=True, texVertex=text, img=image)
+                    tri = []
+                    continue
+                tri.append(vertex[v])
+                text.append(textura[t])
